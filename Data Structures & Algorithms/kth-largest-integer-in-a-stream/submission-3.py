722452cc85e8e33f4084af560d12107fca64d646
iam_biswajeet <112242334+iam-biswajeet@users.noreply.github.com>
2026-04-11 23:52:56 +0530

    Add: kth-largest-integer-in-a-stream - submission-3

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-3.py
@@ -0,0 +1,15 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.li=nums
+        self.idx=k
+
+        
+
+    def add(self, val: int) -> int:
+        self.li.append(val)
+        self.li.sort()
+        print(self.li)
+        return self.li[len(self.li)-self.idx]
+
+        
